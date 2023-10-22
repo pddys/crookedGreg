6c6f9016079a9ebbd1380b83c0e2eba04beb9985
Patrick Sherlock <info@psherlock.co.uk>
2023-10-22 20:52:51 +0100

    FIX: image config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,10 +19,7 @@ export default defineNuxtConfig({
   image: {
     format: 'avif',
     dir: 'public/images',
-    provider: 'netlify',
-    netlify: {
-      baseURl: process.env.IMAGES_URL
-    },
+    provider: 'ipx',
     screens: {
       'xs': 320,
       'sm': 640,
@@ -36,4 +33,5 @@ export default defineNuxtConfig({
   build: {
     transpile: ['gsap'],
   },
+
 })
